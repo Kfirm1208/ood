6726d99be0950b86a17bd7e21c4b177642a1a8a4
K_firm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-08-09 17:56:17 +0700

    restyle the code

--- linkedlist/linkedlist.py
+++ linkedlist/linkedlist.py
@@ -53,44 +53,27 @@ class SinglyLinkedList:
             index += 1
         return -1
     
-    def insert(self,index,data):
-        new_node = Node(data)
-        if index == 0:
-            new_node.next = self.head
-            self.head = new_node
-        else:
-            cur = self.head
-            for _ in range(index - 1):
-                if not cur:
-                    raise IndexError("Index out of range")
-                cur = cur.next 
-            
-            new_node.next = cur.next
-            cur.next = new_node
                 
     def pop(self,pos=None):
         if not self.head:
             return "Out of range"
         
-        if pos is None:
-            popped_data = self.head.data
-            self.head = self.head.next
-            return popped_data
-        
-        elif pos == 0 :
-            popped_data = self.head.data
-            self.head = self.head.next
-            return popped_data
-        else:
-            cur = self.head 
-            # for _ in range(pos-1):
-            #     raise IndexError("Index out of range")
-            cur =  cur.next
-            
-            popped_data = self.head.next.data
-            self.head = self.head.next.next
-            return popped_data
-        
+        else: 
+            if pos > self.size():
+                return "Out of range"
+            else:
+                cur = self.head 
+                if pos == 0:
+                    self.head = self.head.next
+                else:
+                    while cur.next :
+                        if pos == 1:
+                            cur.next = cur.next.next
+                        else:
+                             cur = cur.next
+                    cur.next = None
+            return "Success"
+                                
     def reverse(self):
         cur = self.head
         prev = None
@@ -128,7 +111,7 @@ class SinglyLinkedList:
     
 
 
-class DoubleLinkedList:
+class DoublyLinkedList:
     def __init__(self):
         self.head = None
         self.tail = None
@@ -136,121 +119,130 @@ class DoubleLinkedList:
     def __str__(self):
         if self.isEmpty():
             return "Empty"
-        cur, s = self.head, str(self.head.data) + " "
-        while cur.next != None:
-            s += str(cur.next.data) + " "
+        cur, s = self.head, str(self.head.value) + " "
+        while cur.next:
+            s += str(cur.next.value) + " "
             cur = cur.next
         return s
-    
-    def append(self, data):
-        new_node = Node(data)
-        # don't have data in the linked list
-        if not self.head:
+
+    def reverse(self):
+        if self.isEmpty():
+            return "Empty"
+        cur, s = self.tail, str(self.tail.value) + " "
+        while cur.prev != None:
+            s += str(cur.prev.value) + " "
+            cur = cur.prev
+        return s
+
+    def isEmpty(self):
+        return self.head == None
+
+    def append(self, item):
+        new_node = Node(item)
+        if not self.head :
             self.head = new_node
             self.tail = new_node
-        else:
-            self.tail.next = new_node
-            self.tail = new_node
+            return
+        self.tail.next = new_node
+        new_node.prev = self.tail
+        self.tail = new_node
+          
+           
 
-    def addHead(self, data):
-        new_node = Node(data)
+    def addHead(self, item):
+        new_node = Node(item)
+        if not self.head:
+            self.head = self.tail = new_node
+            return 
+        self.head.prev = new_node
         new_node.next = self.head
         self.head = new_node
-                
-    def search(self,data):
-        cur = self.head
-        while cur:
-            if cur.data == data:
-                return "Found"
-            cur = cur.next 
-        return "Not Found"
-    
-    def size(self):
-        cur = self.head
-        count = 0
-        while cur :
-            count += 1 
-            cur = cur.next 
-        return count 
-    
-    def index(self,data):
-        cur = self.head
-        index = 0 
-        while cur:
-            if cur.data == data:
-                return index
-            cur = cur.next 
-            index += 1 
-
-    def delete(self,data):
-        cur = self.head
-        prev = None
-        while cur:
-            if cur.data == data:
-                if prev:
-                    prev.next = cur.next
-                else:
-                    self.head = cur.next
-                return "Deleted"
-            prev = cur
-            cur = cur.next
-        return "Not Found"
-    
-    def pop(self,pos=None):
-        if not self.head:
-            return "Out of range"
-        
-        if pos is None:
-            popped_data = self.head.data
-            self.head = self.head.next
-            return popped_data
-        
-        elif pos == 0 :
-            popped_data = self.head.data
-            self.head = self.head.next
-            return popped_data
-        else:
-            cur = self.head 
-            for _ in range(pos-1):
-                raise IndexError("Index out of range")
-            cur =  cur.next
-            
-            popped_data = self.head.next.data
-            self.head = self.head.next.next
-            return popped_data
         
 
     def insert(self, pos, item):
-        new_node = Node(item)
-        
-        if pos == 0:
-            new_node.next = self.head
-            if self.head:
-                self.head.prev = new_node
-            self.head = new_node
-        elif pos > 0:
-            cur = self.head
-            for _ in range(pos - 1):
-                if not cur:
-                    raise IndexError("Index out of range")
-                cur = cur.next
-            new_node.next = cur.next
-            if cur.next:
-                cur.next.prev = new_node
-            cur.next = new_node
-            new_node.prev = cur
-        else:
-            cur = self.tail
-            for _ in range(pos - 1):
-                if not cur:
-                    raise IndexError("Index out of range")
-                cur = cur.prev
-            new_node.next = cur.next
-            if cur.next:
-                cur.next.prev = new_node
-            cur.next = new_node
-            new_node.prev = cur
+        if pos >= self.size():
+            self.append(item)
+            return
+        elif pos <= -self.size():
+            self.addHead(item)
+            return
 
+        new_node = Node(item)
+        p = -self.size()
+        cur = self.head
+        while p != pos:
+            cur = cur.next
+            if cur == None: 
+                cur = self.head
+            p += 1
+        cur.prev.next = new_node
+        new_node.next = cur
+        new_node.prev = cur.prev
+        cur.prev = new_node
+
+    def search(self, item):
+        cur = self.head 
+        while cur :
+            if cur.value == item:
+                return "Found"
+            cur = cur.next
+        return "Not Found"
+    
+    def index(self, item):
+        cur = self.head 
+        i = 0 
+        while cur: 
+            if cur.value == item:
+                return i
+            cur = cur.next
+            i += 1
+        return -1
+
+    def size(self):
+        cur = self.head 
+        i = 0 
+        while cur: 
+            i += 1
+            cur = cur.next
+        return i
+
+   
+    def pop(self, pos):
+        length = self.size()
+
+        if not (-length <= pos < length):
+            return "Out of Range"
+
+        if pos < 0:
+            pos += length
+
+        cur = self.head
+        p = 0
+
+        while p != pos:
+            p += 1
+            cur = cur.next
+
+            if cur is None:
+                cur = self.head
+
+        if cur == self.head:
+            self.head = self.head.next
+            if self.head is not None:
+                self.head.prev = None
+            cur.next = None
+            return "Success"
+
+        if cur == self.tail:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            cur.prev = None
+            return "Success"
+
+        cur.prev.next = cur.next
+        cur.next.prev = cur.prev
+        return "Success"
+    
 list = SinglyLinkedList()
 list.append(1)
 list.append(2)
