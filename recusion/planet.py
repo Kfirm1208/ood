cdc61bd77076c5a93db7be6b46b1db8da54b0b71
K_firm1208 <106590971+Kfirm1208@users.noreply.github.com>
2023-08-17 20:35:18 +0700

    update:

--- recusion/planet.py
+++ recusion/planet.py
@@ -1,10 +1,27 @@
-def asteroid_collision(asts):
+def is_collide(l_arts,r_arts):
+    return l_arts >0 and r_arts < 0
+
+def asteroid_collision(asts,index=-1):
     n = len(asts)
-    if -(asts[n]) == asts[n-1] or asts[n] == -(asts[n-1]):
-        pass
-    if asts[n] < (asts[n-1]):
-        pass
+    if n + index <= 0:
+        return asts
+    if is_collide(asts[index-1],asts[index]):
+        fac = 0
+        if abs(asts[index-1]) > abs(asts[index]):
+            asts.pop(index)
+        elif abs(asts[index-1]) < abs(asts[index]):
+            asts.pop(index-1)
+        else:
+            asts.pop(index)
+            asts.pop(index)
+            fac = 2
+        if index != -1:
+            return asteroid_collision(asts,-1)
+        return asteroid_collision(asts,index-fac)
+    else:
+        return asteroid_collision(asts,index-1)
 
 x = input("Enter Input : ").split(",")
 x = list(map(int,x))
 print(asteroid_collision(x))
+
